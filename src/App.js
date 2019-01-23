5698218c0dc80dd151be196632585d6daee52d85
Luis Avila <luis.avila@globant.com>
2019-01-22 18:10:06 -0600

    Adding Authentication on Firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from '../src/hoc/Layout/Layout';
 import Checkout from '../src/containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
 
 class App extends Component {
+    componentDidMount () {
+        this.props.OnTryAutoSignup();
+    }
     render() {
         return (
             <div>
@@ -16,6 +23,8 @@ class App extends Component {
                     <Switch>
                         <Route path="/checkout" component={Checkout} />
                         <Route path="/orders" component={Orders} />
+                        <Route path="/auth" component={Auth} />
+                        <Route path="/logout" component={Logout} />
                         <Route path="/" exact component={BurgerBuilder} />
                     </Switch>
                 </Layout>
@@ -24,4 +33,10 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+    return {
+        OnTryAutoSignup: () => dispatch(actions.authCheckState())
+    };
+};
+
+export default connect(null, mapDispatchToProps)(App);
